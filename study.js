020a602117da41f5b02f981afa4e869a7d50adc3
Takashi <th2826@columbia.edu>
2021-08-14 18:44:19 -0400

    Completed

---
+++ study.js
@@ -0,0 +1,41 @@
+const myVar = "Foo";
+
+//two functions in global scope
+
+function first() {
+  console.log("Inside first()");
+
+  console.log("myVar is currently equal to:", myVar);
+}
+
+function second() {
+  const myVar = "Bar";
+
+  first();
+}
+
+second();
+//=> Inside first()
+//=> myVar is currently equal to: Foo
+
+
+
+/*
+//second is in global scope and first is inside second
+
+function second() {
+  function first() {
+    console.log("Inside first()");
+
+    console.log("myVar is currently equal to:", myVar);
+  }
+
+  const myVar = "Bar";
+
+  first();
+}
+
+second();
+//=> Inside first()
+//=> myVar is currently equal to: Bar
+*/
